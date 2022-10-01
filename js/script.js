de20df8770cf8f8ecd672f2bcb3b5bc70f16e87b
Андрей Немов <cooperationandrey@gmail.com>
2022-10-01 16:58:19 +0300

    add button call

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ $(document).ready(function(){
         $('.nav__burger,.nav__menu').toggleClass('_burger');
         $('body').toggleClass('_lock');
     });
+    $('.nav__link').click(function(event){
+        $('.nav__burger,.nav__menu').toggleClass('_burger');
+        $('body').toggleClass('_lock');
+    });
+    $('.call__hide').click(function(event){
+        $('.call__button,.call__hide').toggleClass('_hide');
+        $('.call__img-hide').toggleClass('_show');
+    });
 
     $('.big-slider').slick({
         arrows: true,
